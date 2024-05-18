3a378c551d130226d4c0e25bb9ef002224774e91
MeAqua1420 <avawaava@gmail.com>
2024-05-18 22:21:15 +0800

    requirement5

--- src/Graph.java
+++ src/Graph.java
@@ -7,6 +7,54 @@ public class Graph {
 
     int table[][] = new int[100][100];
 
+    int a[][] = new int[100][100];
+
+    int path[][] = new int[100][100];
+
+    public String getPath(int[][] path, int i, int j,int [][]table) {
+
+        if (path[i][j] == -1 && table[i][j] != 0) {
+            return " "+i+" "+j;
+        } else if(path[i][j] != -1){
+            int k = path[i][j];
+            return getPath(path, i, k,table)+" "+getPath(path, k, j,table)+" ";
+        }
+        else {
+            return " ";
+        }
+    }
+    public void floyd(){
+        for (int k = 0; k < V; k++) {
+            for (int i = 0; i < V; i++) {
+                for (int j = 0; j < V; j++) {
+                    //a[i][j] = Math.min(a[i][j], a[i][k] + a[k][j]);
+                    if(a[i][k] + a[k][j] < a[i][j]){
+                        path[i][j] = k;
+                        a[i][j] = a[i][k] + a[k][j];
+                    }
+                }
+            }
+        }
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                System.out.println(i + " " + j + ":" + a[i][j]);
+            }
+        }
+    }
+    public void init(){
+        for(int i = 0;i<100;i++){
+            for(int j = 0;j<100;j++){
+                path[i][j] = -1;
+                if(table[i][j] != 0){
+                    a[i][j] = table[i][j];
+                }
+                else{
+                    a[i][j] = Integer.MAX_VALUE >> 1;
+                }
+            }
+        }
+    }
+
     public int AddNode(String prev,String value){
         if(List.indexOf(value) == -1){
             List.add(value);
@@ -14,8 +62,16 @@ public class Graph {
                 int indexP = List.indexOf(prev);
                 int indexN = List.indexOf(value);
                 table[indexP][indexN]++;
+                E++;
             }
             V++;
+        }
+        else{
+            if(V != 0){
+                int indexP = List.indexOf(prev);
+                int indexN = List.indexOf(value);
+                table[indexP][indexN]++;
+            }
             E++;
         }
         return 0;
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,40 @@
 import javax.swing.*;
 import java.util.LinkedList;
+import java.util.Objects;
 import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 public class Main {
+    public static boolean isEmptyOrWhitespace(String str) {
+        return str == null || str.trim().isEmpty();
+    }
+    public static void calcShortestPath(String word1,String word2,Graph graph){
+        String Path = graph.getPath(graph.path, graph.List.indexOf(word1),graph.List.indexOf(word2),graph.table );
+        Path = Path.trim();
+        if(Objects.equals(word1, word2)){
+            System.out.printf(" %s -> %s\n",word1,word2);
+            return;
+        }
+        if(isEmptyOrWhitespace(Path)== true){
+            System.out.printf(" %s -> %s have no road\n",word1,word2);
+        }
+        else{
+            String [] Path_S = Path.split("\\s+");
+
+            for(int i = 0;i<Path_S.length-1;i++){
+                if(Objects.equals(Path_S[i], Path_S[i + 1])){
+                    i++;
+                }
+                System.out.printf(" %s ->",graph.List.get(Integer.valueOf(Path_S[i])));
+            }
+            System.out.printf(" %s ",graph.List.get(Integer.valueOf(Path_S[Path_S.length-1])));
+            System.out.printf("\n");
+        }
+
+
+
+    }
 
     public static void BridgeWord(String left, String right, Graph g){
         LinkedList<Integer> Bridge =  g.Bridge(left,right);
@@ -61,17 +91,28 @@ public class Main {
                     i++;
                 }
                 System.out.println("num of Node = "+graph.V);
-//                 for(int k = 0;k<=20;k++){
-//                    for(int j = 0;j<=20;j++){
-//                        System.out.printf("%d ",graph.table[k][j]);
-//                    }
-//                    System.out.println(" ");
-//                }
+                 for(int k = 0;k<=20;k++){
+                    for(int j = 0;j<=20;j++){
+                        System.out.printf("%d ",graph.table[k][j]);
+                    }
+                    System.out.println(" ");
+                }
             }
         }
         catch (IOException e){
             System.out.println("IOErr");
         }
+        graph.init();
+        graph.floyd();
+        for(int k = 0;k<=20;k++){
+            for(int j = 0;j<=20;j++){
+                System.out.printf("%d ",graph.path[k][j]);
+            }
+            System.out.println(" ");
+        }
+//
+
+
 //        BridgeWord("To","out",graph);
 
 /*         JFrame frame = new JFrame("Drawing Example");
@@ -84,8 +125,20 @@ public class Main {
         // 显示窗口
         frame.setVisible(true);
 */
-        System.out.println("请输入待补全的句子：");
-        String newtxt = Input.nextLine();
-        generateNewText(newtxt,graph);   
+//        System.out.println("请输入待补全的句子：");
+//        String newtxt = Input.nextLine();
+//        generateNewText(newtxt,graph);
+
+        System.out.println("计算最短路径，请输入起点和终点");
+        String shortest = Input.nextLine();
+        String[] newdata = shortest.split(" ");
+        if(newdata.length == 2){
+            calcShortestPath(newdata[0],newdata[1],graph);
+        }
+        if(newdata.length == 1){
+            for(int i = 0;i< graph.V;i++){
+                calcShortestPath(newdata[0],graph.List.get(i),graph);
+            }
+        }
     }
 }
